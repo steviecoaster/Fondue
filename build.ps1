eaeb42ded11868eb9749efc0520556c2243ad21d
Stephen Valdinger <stephen@chocolatey.io>
2024-06-09 17:24:35 -0400

    [skip ci] cleanup

--- build.ps1
+++ build.ps1
@@ -7,87 +7,28 @@ param(
     [string]$SemVer = $(
         if (Get-Command gitversion -ErrorAction SilentlyContinue) {
             gitversion /showvariable SemVer
-        }
-        else {
+        } else {
             '0.1.0'
         }
     ),
 
     # Returns the built module
-    [switch]$PassThru,
-
-    [Parameter()]
-    [switch]
-    $Build,
-
-    [Parameter()]
-    [switch]
-    $RunTest,
-
-    [Parameter()]
-    [switch]
-    $WriteDocs,
-
-    [Parameter()]
-    [switch]
-    $PublishModule
+    [switch]$PassThru
 )
 
+Build-Module -SemVer $SemVer -Passthru:$PassThru -OutVariable build
 
 $scriptRoot = Split-Path -parent $MyInvocation.MyCommand.Definition
 $source = Join-Path $scriptRoot -ChildPath 'source'
-$tests = Join-Path $scriptRoot -ChildPath 'tests'
 $docs = Join-Path $scriptRoot -ChildPath 'docs'
-
+$testPath = Join-Path $source -ChildPath 'module_tests'
+Copy-Item $testPath -Recurse -Destination $build.ModuleBase
 $moduleFolder = Join-Path $scriptRoot -ChildPath $build.Version
+
 $psd1 = Join-Path $moduleFolder -ChildPath 'Chocolatier.psd1'
 
-#Copy the tests that ship with the module to the module folder
-$moduletestPath = Join-Path $source -ChildPath 'module_tests'
-Copy-Item $moduletestPath -Recurse -Destination $build.ModuleBase
+Publish-Module -Path $moduleFolder -Repository PowerShell -NuGetApiKey 605022a4-ab59-33c0-998c-7b60b5c6e801
 
-if ($PublishModule) {
-    Publish-Module -Path $moduleFolder -Repository PowerShell -NuGetApiKey 605022a4-ab59-33c0-998c-7b60b5c6e801
-}
+Import-Module $psd1 -Force
 
-switch ($true) {
-
-    $Build {
-        Build-Module -SemVer $SemVer -Passthru:$PassThru -OutVariable build
-        Import-Module $psd1 -Force
-    }
-    $RunTest {
-
-            $Containers = (Get-Childitem $tests -Filter *.tests.ps1).FullName | Foreach-object { New-PesterContainer -Path $_ -Data $data }
-
-            $Configuration = [PesterConfiguration]@{
-                Run        = @{
-                    Container = $Containers
-                    Passthru  = $true
-                }
-                Output     = @{
-                    Verbosity = 'Detailed'
-                }
-                TestResult   = @{
-                    Enabled      = $true
-                    OutputPath   = "$root\TestResults.xml"
-                    OutputFormat = "JUnitXml"
-    
-            }
-        
-            $results = Invoke-Pester -Configuration $Configuration
-
-
-        }
-    
-        if (Test-Path $TestConfiguration.Run.Path.Value) {
-            Invoke-Pester -Configuration $Configuration
-        }
-    }
-
-    $WriteDocs {
-        New-MarkdownHelp -Module Chocolatier -OutputFolder $docs
-    }
-}
-
-
+New-MarkdownHelp -Module Chocolatier -OutputFolder $docs
