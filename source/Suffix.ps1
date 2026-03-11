312cd4af2bab86a6158b59be3f4b4017966607a8
Stephen Valdinger <stephen@chocolatey.io>
2026-03-10 22:40:48 -0400

    Add a prefix file
    We don't want our check in a suffix, but rather a prefix so it is the first thing that runs

--- source/Suffix.ps1
+++
@@ -1,5 +0,0 @@
-$currentPrincipal = [Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]::GetCurrent()
-
-if (-not $currentPrincipal.IsInRole([Security.Principal.WindowsBuiltInRole]::Administrator)) {
-    throw 'Fondue must be imported in an elevated (Administrator) PowerShell session.'
-}
